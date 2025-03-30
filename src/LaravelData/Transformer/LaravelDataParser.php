8254ab01cd634839e27b795e49c991cb7c12ed0f
Saibotk <git@saibotk.de>
2025-03-30 13:10:05 +0200

    wip: laravel-data refactor & lazy support

--- src/LaravelData/Transformer/LaravelDataParser.php
+++ src/LaravelData/Transformer/LaravelDataParser.php
@@ -11,13 +11,20 @@ use djfhe\PHPStanTypescriptTransformer\Base\Types\TsObjectPropertyType;
 use djfhe\PHPStanTypescriptTransformer\Base\Types\TsObjectType;
 use djfhe\PHPStanTypescriptTransformer\TsTransformer;
 use PHPStan\Analyser\OutOfClassScope;
+use PHPStan\Reflection\AttributeReflection;
+use PHPStan\Type\UnionType;
+use PHPStan\Type\VerbosityLevel;
+use ReflectionClass;
 use ReflectionProperty;
+use Spatie\LaravelData\Support\DataClass;
+use Spatie\LaravelData\Support\DataConfig;
+use Spatie\LaravelData\Support\Lazy\ClosureLazy;
 
 class LaravelDataParser implements TsTypeTransformerContract
 {
     public static function canTransform(Type $type, Scope $scope, ReflectionProvider $reflectionProvider): bool {
 
-      if (!$reflectionProvider->hasClass('Spatie\LaravelData\Data')) {
+      if (! $reflectionProvider->hasClass('Spatie\LaravelData\Contracts\BaseData')) {
         return false;
       }
 
@@ -25,7 +32,7 @@ class LaravelDataParser implements TsTypeTransformerContract
         return false;
       }
 
-      if (!$type->isEnum()->no()) {
+      if (! $type->isEnum()->no()) {
         return false;
       }
 
@@ -35,47 +42,18 @@ class LaravelDataParser implements TsTypeTransformerContract
         return false;
       }
 
-      if (!$reflection->isSubclassOfClass($reflectionProvider->getClass('Spatie\LaravelData\Data'))) {
+      if (! $reflection->isSubclassOfClass($reflectionProvider->getClass('Spatie\LaravelData\Contracts\BaseData'))) {
         return false;
       }
 
       return true;
     }
 
-    /**
-     * @var ?string[]
-     */
-    protected static ?array $laravelDataInternalPropertiesCache = null;
-
-    /**
-     * @return string[]
-     */
-    protected static function getLaravelDataInternalProperties(ReflectionProvider $provider): array
-    {
-      if (self::$laravelDataInternalPropertiesCache !== null) {
-        return self::$laravelDataInternalPropertiesCache;
-      }
-
-      $reflection = $provider->getClass('Spatie\LaravelData\Data');
-
-      $propertyNames = array_map(fn(ReflectionProperty $property) => $property->getName(), $reflection->getNativeReflection()->getProperties());
-      self::$laravelDataInternalPropertiesCache = $propertyNames;
-
-      return self::$laravelDataInternalPropertiesCache;
-    }
-
-    /**
-     * @param string[] $propertyNames
-     * @return string[]
-     */
-    protected static function filterLaravelDataInternalProperties(array $propertyNames, ReflectionProvider $reflectionProvider): array {
-      $internalProperties = self::getLaravelDataInternalProperties($reflectionProvider);
-
-      return array_filter($propertyNames, fn(string $name) => !in_array($name, $internalProperties, true));
-    }
-
     public static function transform(Type $type, Scope $scope, ReflectionProvider $reflectionProvider): TsObjectType
     {
+
+      
+
       /** @var \PHPStan\Type\ObjectType $type */
 
       $reflection = $type->getClassReflection();
@@ -84,17 +62,38 @@ class LaravelDataParser implements TsTypeTransformerContract
         throw new \Exception('Reflection is null');
       }
 
-      $properties = [];
-      
-      $nativePropertyNames = array_map(fn(ReflectionProperty $property) => $property->getName(), $reflection->getNativeReflection()->getProperties());
-      $nativePropertyNames = self::filterLaravelDataInternalProperties($nativePropertyNames, $reflectionProvider);
-      $accessScope = new OutOfClassScope();
-      foreach ($nativePropertyNames as $name) {
-        $property = $reflection->getProperty($name, $accessScope);
-        $properties[] = self::parseProperty($name, $property, $scope, $reflectionProvider);
-      }
+      /** @var DataConfig $dataConfig */
+      $dataConfig = app(DataConfig::class);
 
-      $properties = self::mapPropertyNames($properties, $type, $scope, $reflectionProvider);
+      $dataClass = $dataConfig->getDataClass($type->getClassName());
+
+      // TODO: Implement our own Hidden / Optional Attributes
+      $isOptional = $dataClass->attributes->has('Spatie\TypeScriptTransformer\Attributes\Optional');
+      
+      $properties = [];
+
+      /** @var ReflectionClass<\Spatie\LaravelData\Data> $nativeReflection */
+      $nativeReflection = $reflection->getNativeReflection();
+      
+      $nativeProperties = array_filter(
+          $nativeReflection->getProperties(ReflectionProperty::IS_PUBLIC),
+          fn (ReflectionProperty $property) => ! $property->isStatic()
+      );
+
+      $accessScope = new OutOfClassScope();
+
+    
+      foreach ($nativeProperties as $nativeProperty) {
+        $name = $nativeProperty->getName();
+
+        $property = $reflection->getProperty($name, $accessScope);
+
+        $parsedProperty = self::parseProperty($name, $property, $scope, $reflectionProvider, $dataClass, $isOptional);
+
+        if ($parsedProperty !== null) {
+          $properties[] = $parsedProperty;
+        } 
+      }
 
       $parsed = new TsObjectType($properties);
       $parsed->setName($type->getClassName());
@@ -102,41 +101,36 @@ class LaravelDataParser implements TsTypeTransformerContract
       return $parsed;
     }
 
-    /**
-     * @param array<TsObjectPropertyType> $properties
-     * @return array<TsObjectPropertyType>
-     */
-    protected static function mapPropertyNames(array $properties, \PHPStan\Type\ObjectType $type, Scope $scope, ReflectionProvider $reflectionProvider): array {
-      $attributes = $type->getClassReflection()?->getAttributes() ?? [];
-      $nameMapper = self::parseNameMappingAttribute($attributes, $scope, $reflectionProvider);
-      
-      if ($nameMapper === null) {
-        return $properties;
-      }
-
-      foreach ($properties as $property) {
-        $property->key = $nameMapper($property->key);
-      }
-
-      return $properties;
-    }
-
-    protected static function parseProperty(String $name, \PHPStan\Reflection\ExtendedPropertyReflection $property, Scope $scope, ReflectionProvider $reflectionProvider): TsObjectPropertyType {
+    protected static function parseProperty(string $name, \PHPStan\Reflection\ExtendedPropertyReflection $property, Scope $scope, ReflectionProvider $reflectionProvider, DataClass $dataClass, bool $isOptional): TsObjectPropertyType|null {
       $attributes = $property->getAttributes();
-      $type = self::parseLiteralTypescriptAttribute($attributes, $scope, $reflectionProvider);
-      $nameMapper = self::parseNameMappingAttribute($attributes, $scope, $reflectionProvider);
 
-      if ($nameMapper !== null) {
-        $name = $nameMapper($name);
+      // TODO: This could maybe be simplified by using the $dataProperty->attributes as below. Need to check why this is done in the original code
+      $isHidden = array_any($attributes, fn (AttributeReflection $attribute) => $attribute->getName() === 'Spatie\TypeScriptTransformer\Attributes\Hidden');
+
+      if ($isHidden) {
+          return null;
       }
 
-      if ($type !== null) {
-        return new TsObjectPropertyType($name, $type);
+      $tsType = self::parseLiteralTypescriptAttribute($attributes, $scope, $reflectionProvider);
+
+      /** @var \Spatie\LaravelData\Support\DataProperty $dataProperty */
+      $dataProperty = $dataClass->properties[$name];
+
+      $isOptional = $isOptional
+          || $dataProperty->attributes->has('Spatie\TypeScriptTransformer\Attributes\Optional')
+          || ($dataProperty->type->lazyType !== null && $dataProperty->type->lazyType !== ClosureLazy::class)
+          || $dataProperty->type->isOptional;
+
+      $propertyName = $dataProperty->outputMappedName ?? $dataProperty->name;
+
+      if ($tsType === null) {
+        dump("Transforming " . $dataClass->name . '->' . $name . " => " . $property->getReadableType()->describe(VerbosityLevel::typeOnly()));
+
+
+        $tsType = TsTransformer::transform($property->getReadableType(), $scope, $reflectionProvider);
       }
 
-      $tsType = TsTransformer::transform($property->getReadableType(), $scope, $reflectionProvider);
-
-      return new TsObjectPropertyType($name, $tsType);
+      return new TsObjectPropertyType($propertyName, $tsType, $isOptional);
     }
 
     /**
@@ -163,100 +157,6 @@ class LaravelDataParser implements TsTypeTransformerContract
       assert($arg !== null);
 
       return TsTransformer::transform($arg, $scope, $reflectionProvider);
-    }
-
-    /**
-     * @var string[]
-     */
-    protected static array $laravelDataAttributeNameMapper = [
-      'Spatie\LaravelData\Attributes\MapName',
-      'Spatie\LaravelData\Attributes\MapInputName',
-      'Spatie\LaravelData\Attributes\MapOutputName',
-    ];
-
-    /**
-     * @param \PHPStan\Reflection\AttributeReflection[] $attributes
-     * @return ?\Closure(string): string
-     */
-    protected static function parseNameMappingAttribute(array $attributes, Scope $scope, ReflectionProvider $reflectionProvider): ?\Closure
-    {
-      if (count($attributes) === 0) {
-        return null;
-      }
-
-      /**
-       * @var \PHPStan\Reflection\AttributeReflection[]
-       */
-      $mapInputNameAttribute = array_filter($attributes, fn($attribute) => in_array($attribute->getName(), self::$laravelDataAttributeNameMapper, true));
-
-      if (count($mapInputNameAttribute) === 0) {
-        return null;
-      }
-
-      $first = $mapInputNameAttribute[0];
-
-      $arg = $first->getArgumentTypes()['output'] ?? $first->getArgumentTypes()['input'] ?? null;
-
-      assert ($arg !== null);
-
-      if ($arg instanceof \PHPStan\Type\Constant\ConstantStringType) {
-        $argValue = $arg->getValue();
-
-        if ($argValue === 'Spatie\LaravelData\Mappers\CamelCaseMapper') {
-          return fn(string $name) => lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $name))));
-        }
-
-        if ($argValue === 'Spatie\LaravelData\Mappers\LowerCaseMapper') {
-          return fn(string $name) => strtolower($name);
-        }
-
-        if ($argValue === 'Spatie\LaravelData\Mappers\UpperCaseMapper') {
-          return fn(string $name) => strtoupper($name);
-        }
-
-        if ($argValue === 'Spatie\LaravelData\Mappers\SnakeCaseMapper') {
-          return function (string $name) {
-            $snake = preg_replace('/([a-z])([A-Z])/', '$1_$2', $name); // lower followed by upper
-
-            if ($snake === null) {
-              return $name;
-            }
-
-            $snake = preg_replace('/([A-Z])([A-Z][a-z])/', '$1_$2', $snake); // acronym followed by normal word
-            
-            if ($snake === null) {
-              return $name;
-            }
-
-            // Convert to lowercase
-            $snake = strtolower($snake);
-
-            return $snake;
-          };
-        }
-
-        if ($argValue === 'Spatie\LaravelData\Mappers\StudlyCaseMapper') {
-          return fn(string $name) => str_replace(' ', '', ucwords(str_replace('_', ' ', $name)));
-      }
-      }
-
-      if ($arg instanceof \PHPStan\Type\ObjectType && $arg->getClassName() === 'Spatie\LaravelData\Mappers\ProvidedNameMapper') {
-        $mapTo = $arg->getClassReflection()?->getNativeProperty('name');
-
-        if ($mapTo === null) {
-          return null;
-        }
-
-        $mapToValue = $mapTo->getReadableType();
-
-        if ($mapToValue instanceof \PHPStan\Type\Constant\ConstantStringType) {
-          return fn(string $name) => $mapToValue->getValue();
-        }
-
-        return null;
-      }
-
-      return null;
     }
 
     public static function transformPriority(Type $type, Scope $scope, ReflectionProvider $reflectionProvider, array $candidates): int
