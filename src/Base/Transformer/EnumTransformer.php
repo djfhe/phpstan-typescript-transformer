1a2e987f804ed93131bb4359a14847afe3936706
Saibotk <git@saibotk.de>
2025-05-01 21:52:40 +0200

    fix(enums): Single cases overwriting the global enum type
    We now check more strictly that the transformer only tries to transform actual enums and not potential enums.
    
    Single enum cases also inherit from the ObjectType class and we cannot make them have the enum class name.
    Otherwise, they will overwrite the enum class type definition with their single case, omitting the other potential cases.
    
    We now only set the name, when the full enum is actually used as a type.
    
    The transformer was also adjusted to not return unnecessary union types when it only encapsulates a single type.

--- src/Base/Transformer/EnumTransformer.php
+++ src/Base/Transformer/EnumTransformer.php
@@ -2,35 +2,32 @@
 
 namespace djfhe\PHPStanTypescriptTransformer\Base\Transformer;
 
+use djfhe\PHPStanTypescriptTransformer\TsType;
 use djfhe\PHPStanTypescriptTransformer\TsTypeTransformerContract;
 use djfhe\PHPStanTypescriptTransformer\Base\Types\TsScalarType;
 use djfhe\PHPStanTypescriptTransformer\Base\Types\TsUnionType;
 use djfhe\PHPStanTypescriptTransformer\TsTransformer;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Type\Enum\EnumCaseObjectType;
 use PHPStan\Type\Type;
 
 class EnumTransformer implements TsTypeTransformerContract
 {
     public static function canTransform(Type $type, Scope $scope, ReflectionProvider $reflectionProvider): bool {
-
       if (!$type instanceof \PHPStan\Type\ObjectType) {
         return false;
       }
 
-      if ($type->isEnum()->no()) {
-        return false;
-      }
-
-      return true;
+      return $type->isEnum()->yes();
     }
 
-    public static function transform(Type $type, Scope $scope, ReflectionProvider $reflectionProvider): TsUnionType {
-
-      /** @var \PHPStan\Type\ObjectType $type */
+    public static function transform(Type $type, Scope $scope, ReflectionProvider $reflectionProvider): TsType {
+      assert($type instanceof \PHPStan\Type\ObjectType);
 
       $cases = $type->getEnumCases();
 
+      /** @var list<TsType> $tsCases */
       $tsCases = [];
       
       for ($i = 0; $i < count($cases); $i++) {
@@ -39,7 +36,19 @@ class EnumTransformer implements TsTypeTransformerContract
         $tsCases[] = $tsType;
       }
 
-      return (new TsUnionType($tsCases))->setName($type->getClassName());
+      if (count($tsCases) === 1) {
+          $tsType = $tsCases[0];
+      } else {
+          $tsType = new TsUnionType($tsCases);
+      }
+
+      // If this is a single enum case, we do not want it to be the globally named type for this enum class
+      if ($type instanceof EnumCaseObjectType) {
+          return $tsType;
+      }
+
+      // This represents all cases of the enum, so we name the type to reuse and reference it
+      return $tsType->setName($type->getClassName());
     }
 
     public static function transformPriority(Type $type, Scope $scope, ReflectionProvider $reflectionProvider, array $candidates): int {
